997faa0c361f174de83500ccd7572dda326caf7e
Kamil Ziemian <kziemianfvt@gmail.com>
2017-05-17 13:21:21 +0200

    Add files via upload

--- przedgrup.py
+++ przedgrup.py
@@ -169,6 +169,7 @@ class GrupaProPun(Grupa):
                  numer_grupy):
         Grupa.__init__(self, nazwa_przedmiotu, semestr, rok_akademicki,
                        numer_grupy)
+
         self.__ilosc_projektow = 0
         self.__projekty_punktacja = []
 
@@ -235,28 +236,82 @@ class GrupaSprPun(Grupa):
 # Klasa Grupa Zestaw Zadań Projekt Ocena
 
 
-class GrupaZesZadProOce(GrupaZesZad):
+class GrupaZesZadProOce(object):
     u"""Grupa, zestaw zadań, projekt za ocenę."""
 
+    # Straszna chała, ale działa.
+    
     def __init__(self, nazwa_przedmiotu, semestr, rok_akad,
                  numer_grupy):
-        self.przed_atrybuty(nazwa_przedmiotu, semestr, rok_akad)
-        self.grupa_atrybuty(nazwa_przedmiotu, semestr, rok_akad, numer_grupy)
-        self.grupa_zes_zad_atry()
-        self.grupa_zes_zad_pro_oce()
+        # self.przed_atrybuty(nazwa_przedmiotu, semestr, rok_akad)
+        # self.grupa_atrybuty(nazwa_przedmiotu, semestr, rok_akad, numer_grupy)
+        # self.grupa_zes_zad_atry()
+        # self.grupa_zes_zad_pro_oce()
 
+        self.__opis_przed = str(nazwa_przedmiotu) + ', semestr ' \
+                            + str(semestr) + ' ' + str(rok_akad) + '\n'
 
-    def grupa_zes_zad_pro_oce(self):
-        self.__licz_proj = 0
-        self.__opis_wyn = "Imie Nazwisko    " \
+        self.__lista_stud = []
+        
+        self.__num_grupy = numer_grupy
+        self.__nazwa_pliku_z_wyn = nazwa_przedmiotu + '-' + semestr \
+                                 + '-' + rok_akad + '-' \
+                                 + 'Grupa-' + str(numer_grupy) \
+                                 + '-Wyniki.txt'
+
+        self.__ilosc_zajec = 0  # Liczba zajęć jaka się odbyła. Liczone są
+        # również te na które nikt nie przyszedł.
+
+        self.__ilosc_zestawow_zadan = 0
+        self.__zestawy_zadan_punktacja = []
+        
+        self.__liczba_proj = 0
+        self.__opis_wyn = "Imie       Nazwisko    " \
                           "Fre. %  Punkty  Oce. sre. z projektow\n"
+
         # Zrób tak, by += w powyższym wzorze działało.
 
 
+    def dodaj_studenta(self, student_inst):
+        u"""Dodaje instację studenda STUDENT_INST do atrybutu
+            __LISTA_STUDENTOW"""
+        self.__lista_stud.append(student_inst)
+
+
+    def odbyly_sie_zajecia(self):
+        u"""Zwiększa atrybut SELF.__ILOSC_ZAJEC o 1."""
+        self.__ilosc_zajec += 1
+
+
+    def bylo_zajec(self):
+        return self.__ilosc_zajec
+
+
+    def byl_zestaw_zadan(self, punktacja_zestawu):
+        u"""Jeśli był nowy zestaw zadań, to punktację zadań dodajemy do listy
+        punktacji.
+
+        Metoda sprawdza, czy lista jest krotką. Krotką, bo raz ustawiona,
+        punktacja ma być niezmienna."""
+        assert isinstance(punktacja_zestawu, tuple), \
+            u"Punktacja zestawu nie jest krotką!"
+
+        self.__ilosc_zestawow_zadan += 1
+        self.__zestawy_zadan_puktacja.append(punktacja_zestawu)
+
+
     def byl_proj(self):
-        self.__ilosc_proj += 1
+        self.__liczba_proj += 1
+
         
     def liczba_proj(self):
-        return self.__ilosc_proj
+        return self.__liczba_proj
 
+
+    def zapisz_do_pliku(self):
+        with open(self.__nazwa_pliku_z_wyn, 'w') as wyniki:
+            wyniki.write(self.__opis_przed)
+            wyniki.write(self.__opis_wyn)
+            for student in self.__lista_stud:
+                wyniki.write(student.wyniki_studenta())
 
--- stud.py
+++ stud.py
@@ -3,8 +3,6 @@
 
 
 
-from przedgrup import *
-
 # Należy poprawić ten notatki według standardów Pythona.
 
 ######################################################################
@@ -36,8 +34,8 @@ class StudentCwi(Student):
 
     def __init__(self, imie_studenta, nazwisko_studenta, przedmiot_inst):
 
-        super(StudentCwi, self).__init__(imie_studenta, nazwisko_studenta,
-                                         przedmiot_inst)
+        # super(StudentCwi, self).__init__(imie_studenta, nazwisko_studenta,
+        #                                  przedmiot_inst)
         self.__obecnosc = 0
         
     
@@ -64,8 +62,8 @@ class StudentCwiZesZad(StudentCwi):
     
     def __init__(self, imie_studenta, nazwisko_studenta, przedmiot_inst):
 
-        StudentCwiczenia.__init__(self, imie_studenta, nazwisko_studenta,
-                                  przedmiot_inst)
+        # StudentCwiczenia.__init__(self, imie_studenta, nazwisko_studenta,
+        #                           przedmiot_inst)
         self.__rozwiazane_zestawy_zadan = []
         
         
@@ -84,8 +82,8 @@ class StudentCwiProOce(StudentCwi):
     
     def __init__(self, imie_studenta, nazwisko_studenta, przedmiot_inst):
 
-        StudentCwiczenia.__init__(self, imie_studenta, nazwisko_studenta,
-                                  przedmiot_inst)
+        # StudentCwiczenia.__init__(self, imie_studenta, nazwisko_studenta,
+        #                           przedmiot_inst)
         self.__oceny_za_projekty = []
         
         
@@ -97,44 +95,81 @@ class StudentCwiProOce(StudentCwi):
 
 ##############################
 
-class StudentCwiZesZadProOce(StudentCwi):
+class StudentCwiZesZadProOce(object):
     u"""Student, ćwiczenia, zestwy zadań i projekty za oceny."""
+
+    # Chała która działa. Popraw.
     
     def __init__(self, imie_studenta, nazwisko_studenta, przedmiot_inst):
+        self.__imie_stud = imie_studenta
+        self.__nazw_stud = nazwisko_studenta
+        self.__przed_inst = przedmiot_inst
+        self.__przed_inst.dodaj_studenta(self)
+        
+        self.__ocena = 'Nic'
+        self.__komentarz = ''
+
+        self.__obecnosc = 0
+        
+        self.__rozwi_zest_zad = []
+
         self.__oceny_za_proj = []
-        super(StudentCwiZesZadProOce, self).__init__(imie_studenta,
-                                                     nazwisko_studenta,
-                                                     przedmiot_inst)
+        # super(StudentCwiZesZadProOce, self).__init__(imie_studenta,
+        #                                              nazwisko_studenta,
+        #                                              przedmiot_inst)
         
         
     def oddany_projekt(self, ocena_za_projekt):
         # punkty_za_zadania mają być krotką
         self.__oceny_za_proj.append(ocena_za_projekt)
 
+
+    def byl_na_zajeciach(self):
+        u"""Zwiększa atrybut __obecność o 1."""
+        
+        self.__obecnosc += 1
+
         
     def oddany_zestaw_zadan(self, punkty_za_zadania):
         # punkty_za_zadania mają być krotką
-        self.__rozwiazane_zestawy_zadan.append(punkty_za_zadania)
+        self.__rozw_zes_zad.append(punkty_za_zadania)
+
+
+    def oddany_projekt(self, ocena_za_projekt):
+        # punkty_za_zadania mają być krotką
+        self.__oceny_za_proj.append(ocena_za_projekt)
+
+    def dodaj_komentarz(self, komentarz):
+        self.__komentarz += komentarz + '\n\n'
+
 
     def wyniki_studenta(self):
-        opis_wynik_stu = self.__imie_studenta.ljust(10) \
-                         + self.__nazwisko_studenta.ljust(10)
+        opis_wynik_stud = self.__imie_stud.ljust(11) \
+                          + self.__nazw_stud.ljust(11)
         # Zmień opis_wynik_stu na opis_wynik_stud
+        ilosc_zajec = self.__przed_inst.bylo_zajec()
 
-        if (self.bylo_zajec() != 0):
-            opis_wynik_stu += "  " + str(100 * float(self.__obecnosc)
-                                         / self.bylo_zajec()) + "  "
+                # print opis_wynik_stud
+
+        if (ilosc_zajec != 0):
+            opis_wynik_stud += "  " \
+                               + ("%.1f") % (100 * float(self.__obecnosc)
+                                             / ilosc_zajec) + "  "
+            opis_wynik_stud = opis_wynik_stud.rjust(5)
         else:
-            opis_wynik_stu = "  Nic  "
+            opis_wynik_stud += "  Nic  "
 
-        ilosc_proj = self.__przed_inst.ilosc_proj()
+        ilosc_proj = self.__przed_inst.liczba_proj()
 
         if (ilosc_proj != 0):
             oce_srd_proj = sum(self.__oceny_za_proj, 0.0) / ilosc_proj
         else:
-            oce_srd_proj = "  Nic\n"
+            oce_srd_proj = "  Nic  "
 
-        opis_wynik_stu += "  " + str(oce_srd_proj) + "\n"
+        opis_wynik_stud += "  " + str(oce_srd_proj) + "\n"
+
+        if (self.__komentarz != ''):
+            opis_wynik_stud += self.__komentarz
 
 
-        return opis_wynik_stu
+        return opis_wynik_stud
